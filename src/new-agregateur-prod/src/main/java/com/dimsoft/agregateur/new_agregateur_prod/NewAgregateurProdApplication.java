8b27a486ce76498ea2b50e2bbcf2a7b3e4e4c779
wilfriedjordan <tankwewilfried5@gmail.com>
2025-10-24 11:28:19 +0100

    initialisation du projet

---
+++ src/new-agregateur-prod/src/main/java/com/dimsoft/agregateur/new_agregateur_prod/NewAgregateurProdApplication.java
@@ -0,0 +1,13 @@
+package com.dimsoft.agregateur.new_agregateur_prod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewAgregateurProdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewAgregateurProdApplication.class, args);
+	}
+
+}
---
+++ src/new-agregateur-prod/src/test/java/com/dimsoft/agregateur/new_agregateur_prod/NewAgregateurProdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dimsoft.agregateur.new_agregateur_prod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewAgregateurProdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
